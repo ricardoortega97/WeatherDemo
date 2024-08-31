9be01eee54d4fe2cc5a34fef99b196505b22f016
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-08-30 21:12:17 -0400

    weather UI added

--- WeatherDemo/Views/WeatherView.swift
+++ WeatherDemo/Views/WeatherView.swift
@@ -11,10 +11,62 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading){
+            VStack {
+                //Top Head
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth:.infinity, alignment: .leading)
+                
+                Spacer()
+                //Icon of current weather body
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "cloud")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                    }
+                    Spacer()
+                        .frame(height: 80)
+                    //Image of the condition of the weather
+                    AsyncImage(url: URL(string: "https://png.pngtree.com/png-vector/20220621/ourmid/pngtree-cloudy-weather-forecast-cloud-overcast-png-image_5246453.png" )) {
+                        image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth:.infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
-#Preview {
-    WeatherView(weather: previewWeather)
+struct WeatherView_Previews: PreviewProvider{
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
 }
